243cccc3246735e8ccae92731ed12528cd52fe09
Lesa1820 <lesa182011@gmail.com>
2020-10-02 10:16:16 +0300

    docs update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
+  if(array.length !== 0){
+    return Math.min.apply( Math, array );
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if(array.length !== 0){
+    return Math.max.apply( Math, array );
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  var x = array.length , sum = 0;
+  for(let i = 0;i < x;i++){
+    sum = sum + Number(array[i])
+  }
+   sum = sum / x;
+  return sum;
 }
+
